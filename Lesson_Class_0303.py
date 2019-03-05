edeb41e9bccad1a1260878cd4525fb4941dbc10e
AlecShang <shangjingweiwork@163.com>
2019-03-05 23:17:08 +0800

    0304 update class StudentPython()

--- Lesson_Class_0303.py
+++ Lesson_Class_0303.py
@@ -8,5 +8,15 @@ class StudentPython():
     student_age = '18'
     student_major = 'computer'
 
+    def DoHomework(self):
+        print('please do homework!')
+        print('then you can play!')
+        
+        return None
+
+
 shang = StudentPython()
 print(shang.student_age)
+print(shang.DoHomework())
+print(StudentPython.__dict__)
+
