fd0589cf569370c1916e3b01a5acdb5cd09a3b32
AlecShang <shangjingweiwork@163.com>
2019-03-18 16:11:58 +0800

    0318 update OOP all

--- 02. OOP/OOP.py
+++ 02. OOP/OOP.py
@@ -46,33 +46,93 @@
 # b = B()
 # print(b._name)
 
+# class Student():
+#     _name = 'shangjingwei'
 
-class Student():
-    def __init__(self):
-        print('this is a Student init operation!')
-        print('this is a student class!')
+#     def __init__(self):
+#         print('this is a Student init operation!')
+#         print('this is a student class!')
+
+#     def work(self):
+#         print('need working...')
+
+# class PythonStudent(Student):
+#     name = None
+#     age = 18
+#     address = 'Yunnan university of finance and economics'
+
+#     __sex = '女'
+
+#     def __init__(self):
+#         self.name = 'shang'
+#         self.age = 20
+
+#         print('this is a PythonStudent init operation!')
+
+#     def say(self):
+#         print('my name is {0} and i am {1}'.format(self.name, __class__.age))
+#         return 1
+
+#     def work(self):
+#         super().work()
+#         self.say()
+
+# python_student = PythonStudent()
+# print(issubclass(Student, PythonStudent))
+# print(PythonStudent._name)
+# print(python_student.age)
+# print(python_student.say())
+# print(python_student.__dict__)
+# print(PythonStudent.__dict__)
+
+# property(fget, fset, fdel, fdoc)的使用
+# 无论输入什么类型的数字,都将其转换为整数
+
+# class Person():
+#     # _name = None
+#     _name = 'aaa'
+
+#     def fget(self):
+#         return self._name * 2
+
+#     def fset(self, name):
+#         self._name = name.upper()
+
+#     def fdel(self):
+#         del self._name
+
+#     def fdoc(self):
+#         print('this is a property')
+
+#     x = property(fget, fset, fdel, fdoc)
+
+# # p.x 将触发 getter,p.x = value 将触发 setter ， del p.x 触发 deleter。
+
+# p = Person()
+# p.x = "shang"
+# print(p.x)
+
+# 三种方法的实现,实例方法;类方法;静态方法
+# class Student():
+#     # 实例方法
+#     def A(self):
+#         pass
+
+#     # 类方法
+#     def B(cls):
+#         pass
+
+#     # 静态方法
+#     def C():
+#         pass
+
+# 抽象类的实现
+import abc
 
 
-class PythonStudent(Student):
-    name = None
-    age = 18
-    address = 'Yunnan university of finance and economics'
-
-    __sex = '女'
-
-    def __init__(self):
-        self.name = 'shang'
-        self.age = 20
-
-        print('this is a PythonStudent init operation!')
-
-    def say(self):
-        print('my name is {0} and i am {1}'.format(self.name, __class__.age))
-        return 1
-
-
-python_student = PythonStudent()
-print(python_student.age)
-print(python_student.say())
-print(python_student.__dict__)
-print(PythonStudent.__dict__)
+# 声明一个元类,并且指定当前类的元类
+class Human(metaclass=abc.ABCMeta):
+    # 定义一个抽象方法
+    @abc.abstractmethod
+    def eating(self):
+        pass
