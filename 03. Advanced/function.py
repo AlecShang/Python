805e8bebf1c5a560b7d13bc5dc5f9975f22f92c9
AlecShang <shangjingweiwork@163.com>
2019-03-19 10:43:06 +0800

    0319 update advanced_function.py

---
+++ 03. Advanced/function.py
@@ -0,0 +1,63 @@
+#!/usr/local/bin/python3
+# -*- encoding: utf-8 -*-
+'''
+@File    :   function.py
+@Time    :   2019/03/18 16:14:07
+@Author  :   Alec Shang
+@Version :   1.0
+@Contact :   shangjingweiwork@163.com
+@License :   (C)Copyright 2019-2020
+@Desc    :   None
+'''
+#######################
+
+# zip; enumerate; collections(dequeue; defaultdict; Counter )
+
+# zip case and zip(*)为解码
+
+n = [1, 2, 3, 4]
+m = [6, 7, 8, 9]
+# print(list(zip(n, m)))
+
+zipped = zip(n, m)
+# print(list(zipped))
+# # print zipped之后, 里边的内容为何会被清空?
+# print(list(zip(*zipped)))
+
+# enumerate case
+
+i = [1, 2, 3, 4, 5, 6, 7]
+l1 = enumerate(i)
+# for j in l1:
+#     print(j)
+
+# collections case
+
+import collections
+Point = collections.namedtuple("Point", ['x', 'y', 'z'])
+p = Point(11, 22, 33)
+# print(p[2])
+
+# deque case
+from collections import deque
+q = deque(['a', 'b', 'c'])
+# print(q)
+
+# q.appendleft('d')
+# print(q)
+
+# q.insert(2, 'e')
+# print(q)
+
+# defaultdict case
+from collections import defaultdict
+
+dic1 = {'one': 1, 'two': 2, 'three': 3}
+func = lambda: '返回字典默认字符'
+dic1 = defaultdict(func)
+print(dic1['one'])
+
+# counter case
+from collections import Counter
+c = Counter('asfoijsgisamviosdmioehwgnweiohgkwlengornher')
+print(c)
