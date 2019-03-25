6267e0c2e643b71203c8a1a1bfe1097584728192
AlecShang <shangjingweiwork@163.com>
2019-03-25 15:06:51 +0800

    0325 update Tensorflow

---
+++ 06. Tensorflow/example.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   example.py
+@Time    :   2019/03/25 15:06:25
+@Author  :   Alec Shang
+@Version :   1.0
+@Contact :   shangjingweiwork@163.com
+@License :   (C)Copyright 2019-2020
+@Desc    :   None
+'''
+#######################
